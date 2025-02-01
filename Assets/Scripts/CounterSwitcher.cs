3763de05563184a61338f5a5722ae8c68f7af206
multilococ <multilococ@yandex.ru>
2025-02-02 01:36:03 +0300

    Final Fixes
    Поправил последние замечания по коду

--- Assets/Scripts/CounterSwitcher.cs
+++ Assets/Scripts/CounterSwitcher.cs
@@ -1,24 +1,37 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CounterSwitcher : MonoBehaviour
 {
     [SerializeField] private AnimationClip _buttonClick;
     [SerializeField] private Animator _buttonAnimator;
     [SerializeField] private Counter _counter;
-    [SerializeField] private GameObject _lockImage;
+    [SerializeField] private Button _button;
+    [SerializeField] private Image _lockImage;
 
     private bool _isLockVisible;
 
     private void Start()
     {
         _isLockVisible = false;
-        _lockImage.SetActive(_isLockVisible);
+        _lockImage.enabled = _isLockVisible;
     }
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(ClickCounterButton);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(ClickCounterButton);
+    }
+
     public void ClickCounterButton()
     {
         _buttonAnimator.Play(_buttonClick.name);
         _counter.SwitchCoroutineState();
         _isLockVisible = !_isLockVisible;
-        _lockImage.SetActive(_isLockVisible);
+        _lockImage.enabled = _isLockVisible;
     }
 }
